5652fe3aa1741dfc26ad0271c312a60fb14df861
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-28 13:18:28 +0000

    ticket 2b potentially completed

--- quote.js
+++ quote.js
@@ -1,24 +1,27 @@
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
+import { readFile } from "node:fs/promises";
 
 const fileName = "quotes.json";
 
 export async function getQuotes() {
+	try {
+		const data = await readFile(fileName, "utf8");
 
+		const jsonData = await JSON.parse(data);
+		console.log(jsonData);
+		return jsonData;
+	} catch (error) {
+		console.error("Error reading file:", error);
+	}
 }
 
-export async function addQuote(quoteText) {
+getQuotes();
 
-}
+export async function addQuote(quoteText) {}
 
-export async function getRandomQuote() {
+export async function getRandomQuote() {}
 
-}
+export async function editQuote(id, quoteText) {}
 
-export async function editQuote(id, quoteText) {
-
-}
-
-export async function deleteQuote(id) {
-
-}
+export async function deleteQuote(id) {}
