bfa7ce3a6a3d72002b62d55178654144c8878130
1ravi48 <ravi48@hotmail.co.uk>
2024-03-02 10:27:37 +0000

    ticket 2c complete

--- quote.js
+++ quote.js
@@ -7,7 +7,7 @@ export async function getQuotes() {
 	try {
 		const data = await fs.readFile(fileName, "utf8");
 
-		const jsonData = await JSON.parse(data);
+		const jsonData = JSON.parse(data);
 		console.log(jsonData);
 		return jsonData;
 	} catch (error) {
@@ -16,7 +16,17 @@ export async function getQuotes() {
 }
 
 export async function addQuote(quoteText) {
-	
+	const jsonData = await fs.readFile(fileName, "utf8");
+	const jsArray = JSON.parse(jsonData);
+	const id = uuidv4();
+	const quoteObject = {
+		id,
+		quoteText
+	};
+	jsArray.push(quoteObject);
+	const newJsonData = JSON.stringify(jsArray);
+	await fs.writeFile(fileName, newJsonData, "utf8");
+	return quoteObject;
 }
 
 export async function getRandomQuote() {}
